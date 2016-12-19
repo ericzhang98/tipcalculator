fbd16346540440b9c85e11327c24500db21fb41b
Eric Zhang <zhangeric99@gmail.com>
2016-12-19 01:58:11 -0800

    split tip functionality

--- tip.php
+++ tip.php
@@ -4,8 +4,6 @@
 
 <head>
   <title>Tip Calculator</title>
-  <!--link rel="stylesheet"
-  href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"-->
 </head>
 
 <body style="background-color:gray">
@@ -15,9 +13,12 @@
 $amount = NULL; //bill amount
 $percentage = 10; //default percentage at 10%
 $customPercentage = NULL; //custom percentage amount
-$amountError = false; //bill amount error flag for bad input
-$customError = false; //custom percentage error flag for bad input
+$split = 1; //default split at 1
+//error flags
 //assume no error for input unless we get an error when processing
+$amountError = false; 
+$customError = false; 
+$splitError = false;
 
 function calculateTip() {
   //if -1, then use customPercentage
@@ -31,22 +32,19 @@ function calculateTip() {
 
 //handle form post request
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $amount = $_POST["amount"];
+  $amount = str_replace(",", "", $_POST["amount"]);
   $percentage = $_POST["percentage"];
   $customPercentage = $_POST["customPercentage"];
-  //set amount error if needed
+  $split = str_replace(",", "", $_POST["split"]);
+  //set amount, custom, and split error if needed
   processAmount($amount);
-  //set custom error if needed
   processCustomPercentage($customPercentage);
+  processSplit($split);
 }
 
 //error flagging for bill 
 function processAmount($data) {
-  if (isset($data) && !is_numeric($data) && !is_int($data) &&
-        !is_float($data)) {
-    $GLOBALS["amountError"] = true;
-  }
-  if ($data <= 0) {
+  if ((isset($data) && !is_numeric($data)) || $data <= 0) {
     $GLOBALS["amountError"] = true;
   }
 }
@@ -55,13 +53,18 @@ function processAmount($data) {
 function processCustomPercentage($data) {
   //we only want to set customError if custom button is checked
   if ($GLOBALS["percentage"] == -1) {
-    if (isset($data) && !is_numeric($data) && !is_int($data) &&
-          !is_float($data)) {
+    if ((isset($data) && !is_numeric($data)) || $data <= 0) {
       $GLOBALS["customError"] = true;
     }
-    if ($data <= 0) {
-      $GLOBALS["customError"] = true;
-    }
+  }
+}
+
+//error flagging for split
+function processSplit($data) {
+  //check if it's greater than 0 and is an int
+  if ((isset($data) && !is_numeric($data)) || $data <= 0 || 
+      (floatval($data) != intval($data))) {
+    $GLOBALS["splitError"] = true;
   }
 }
 
@@ -78,7 +81,7 @@ border-style:solid; border-width: 3px; border-color:green">
   echo htmlspecialchars($_SERVER["PHP_SELF"]);
 ?>">
 
-  <p> Bill subtotal: $ <input type="text" name="amount" 
+  <p> Bill subtotal: $ <input type="text" name="amount" style="width:50px"
   value="<?php echo $amount; ?>"> 
   <?php
   if ($amountError) {
@@ -105,21 +108,40 @@ border-style:solid; border-width: 3px; border-color:green">
   if ($percentage == -1) {
    echo "checked";
   } 
-  echo '> Custom percentage';
-  echo '<input type="text" name="customPercentage" value="', $customPercentage, '">%';
+  echo '> Custom percentage: ';
+  echo '<input type="text" name="customPercentage" style="width:30px" value="', $customPercentage, '">%';
   if ($customError) {
     echo '<p style="color:red"> *Please enter a valid tip percentage</p>';
   }
+
+  //show the split option
+  echo '<p>Number of people: ';
+  echo '<input type="text" name="split" style="width:30px" value="', $split, '">';
+  if ($splitError) {
+    echo '<p style="color:red"> *Please enter a valid number of people</p>';
+  }
+
+
   ?>
   <p> <button type="submit"> Submit </button> </p>
 
 </form>
 
 <?php
-if (isset($amount) && !$amountError && !$customError) {
+//SHOW END TIP RESULT
+if (isset($amount) && !$amountError && !$customError && !$splitError) {
   echo "<h3>Result</h3>";
   echo "<p>Tip amount: $", calculateTip(), "<br>";
   echo "Total: $", $amount + calculateTip(), "</p>";
+
+  //show split tip results if more than 1 person splitting
+  if (!$splitError && $split > 1) {
+    $splitTip = ceil(calculateTip()/$split*100)/100;
+    $splitTotal = ceil(($amount + calculateTip())/$split*100)/100;
+    echo "<h3>Splitting</h3>";
+    echo "<p>Split tip amount: $", $splitTip, "<br>";
+    echo "Split total: $", $splitTotal, "</p>";
+  }
 
   if ($amount >= 1000) {
     echo "<p>Damn, that's a pretty hefty bill. Consider making a donation to ";
